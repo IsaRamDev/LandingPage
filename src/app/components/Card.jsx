3d8540287368d91a057c04ff6061124a119185a4
Isabel Ramirez <isaram1218@gmail.com>
2023-11-26 19:41:29 -0600

    fix: responsive errors

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -5,26 +5,25 @@ import { BsGithub } from "react-icons/bs";
 
 function Card({ title, github, imgUrl, link, stack }) {
   return (
-    <div className="relative overflow-hidden rounded-2xl">
-      <div className="object-cover">
+    <div className="relative overflow-hidden rounded-2xl bg-white shadow-md h-auto">
+      <div className="w-full h-56 relative">
         <a
           href={link}
           target="_blank"
           rel="noopener noreferrer"
-          className="w-full block shadow-2xl"
         >
           <Image
             src={imgUrl}
             alt={`${title} portfolio`}
-            className="lg:hover:animate-pulse lg:hover:animate-infinite lg:hover:animate-duration-1000 lg:hover:animate-ease-linear lg:hover:animate-alternate object-cover h-full w-full"
             width={1280}
             height={720}
-            layout="responsive"
+            objectFit="cover"
+            className="lg:hover:animate-pulse lg:hover:animate-infinite lg:hover:animate-duration-1000 lg:hover:animate-ease-linear lg:hover:animate-alternate object-cover h-full w-full"
           />
         </a>
       </div>
 
-      <div className="absolute flex justify-center items-center w-full flex-wrap gap-2 bottom-14 z-10">
+      <div className="absolute flex justify-center items-center w-full flex-wrap gap-2 p-4 bottom-0">
         {stack.map((item, index) => (
           <Chip key={index} background={getBackground(item)}>
             {item}
@@ -36,10 +35,10 @@ function Card({ title, github, imgUrl, link, stack }) {
         href={github}
         target="_blank"
         rel="noopener noreferrer"
-        className="w-full block shadow-2xl"
+        className="absolute top-0 left-0"
       >
-        <h1 className="absolute flex items-center top-5 right-2 text-gray-50 font-bold text-xl bg-rose-300 hover:bg-rose-200 rounded-lg px-2">
-          <BsGithub className="inline-block mx-2" />
+        <h1 className="flex items-center text-gray-800 font-bold text-lg bg-rose-300 hover:bg-rose-200 rounded-r-lg px-2 py-1">
+          <BsGithub className="mr-2" />
           {title}
         </h1>
       </a>
